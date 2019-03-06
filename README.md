ed6938a165cea80ebc87f126a72c2f7cb6e32719
Daniel Rose <daniel-rose@gmx.de>
2019-03-06 17:48:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Conditional Availability Sales Connector
+
+[![Build Status](https://travis-ci.org/fond-of/spryker-conditional-availability-sales-connector.svg?branch=master)](https://travis-ci.org/fond-of/spryker-conditional-availability-sales-connector)
+[![PHP from Travis config](https://img.shields.io/travis/php-v/symfony/symfony.svg)](https://php.net/)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://packagist.org/packages/fond-of-spryker/conditional-availability-sales-connector)
+
+## Installation
+
+```
+composer require fond-of-spryker/conditional-availability-sales-connector
+```
---
+++ src/FondOfSpryker/Zed/ConditionalAvailabilitySalesConnector/Communication/Plugin/SalesExtension/DeliveryTimeOrderItemExpanderPreSavePlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FondOfSpryker\Zed\ConditionalAvailabilitySalesConnector\Communication\Plugin\SalesExtension;
+
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use Generated\Shared\Transfer\SpySalesOrderItemEntityTransfer;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\SalesExtension\Dependency\Plugin\OrderItemExpanderPreSavePluginInterface;
+
+class DeliveryTimeOrderItemExpanderPreSavePlugin extends AbstractPlugin implements OrderItemExpanderPreSavePluginInterface
+{
+    /**
+     * Specification:
+     *  - Allows to manipulate SpySalesOrderItemEntity transfer object data before storing in Persistence.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
+     * @param \Generated\Shared\Transfer\SpySalesOrderItemEntityTransfer $salesOrderItemEntity
+     *
+     * @return \Generated\Shared\Transfer\SpySalesOrderItemEntityTransfer
+     */
+    public function expandOrderItem(
+        QuoteTransfer $quoteTransfer,
+        ItemTransfer $itemTransfer,
+        SpySalesOrderItemEntityTransfer $salesOrderItemEntity
+    ): SpySalesOrderItemEntityTransfer {
+        $salesOrderItemEntity->setDeliveryTime($itemTransfer->getDeliveryTime());
+
+        return $salesOrderItemEntity;
+    }
+}
